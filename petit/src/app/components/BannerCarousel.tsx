974371bf0772f67b18901e535a5b843ccd98e949
jinseok469 <jinseok469@naver.com>
2025-07-26 21:24:58 +0900

    0726

--- petit/src/app/components/BannerCarousel.tsx
+++ petit/src/app/components/BannerCarousel.tsx
@@ -13,23 +13,24 @@ const BannerCarousel = () => {
       autoplay={{ delay: 2500, disableOnInteraction: false }}
       pagination={{ clickable: true }}  // 아래 점(dot) 표시 및 클릭 가능
       style={{ height: '100%' }}
+        className="w-full h-full"
     >
-      <SwiperSlide className="h-full">
+      <SwiperSlide className="h-full min-h-[300px]">
   <div className="flex items-center justify-center h-full text-center text-sm text-white bg-patit">
     앱 설명 4개 이미지 추가 예정
   </div>
 </SwiperSlide>
-<SwiperSlide className="h-full">
+<SwiperSlide className="h-full min-h-[300px]">
   <div className="flex items-center justify-center h-full text-center text-sm text-white bg-patit">
     앱 설명 4개 이미지 추가 예정
   </div>
 </SwiperSlide>
-<SwiperSlide className="h-full">
+<SwiperSlide className="h-full min-h-[300px]">
   <div className="flex items-center justify-center h-full text-center text-sm text-white bg-patit">
     앱 설명 4개 이미지 추가 예정
   </div>
 </SwiperSlide>
-<SwiperSlide className="h-full">
+<SwiperSlide className="h-full min-h-[300px]">
   <div className="flex items-center justify-center h-full text-center text-sm text-white bg-patit">
     앱 설명 4개 이미지 추가 예정
   </div>
